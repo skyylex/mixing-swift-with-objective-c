b838d3bce39c537bac12632e9aa4fd808676e553
Yury Lapitsky <yury.lapitsky@gmail.com>
2021-02-14 11:40:03 +0100

    Dummy swift explanatory comment

--- mixing-swift-with-objc/Dummy.swift
+++ mixing-swift-with-objc/Dummy.swift
@@ -6,3 +6,8 @@
 //
 
 import Foundation
+
+// Adding .swift file to the Objective-C project adds a Bridging header and
+// forces Xcode to link Swift-libraries, otherwise failing with
+// `Auto-Linking library not found for -lswiftFoundation
+// More info here: https://stackoverflow.com/questions/50096025/it-gives-errors-when-using-swift-static-library-with-objective-c-project/56187043#56187043
